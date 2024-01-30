57f39fb94ddfcf3ea3718a2fd63b349594618c19
Heejung Jang <112256988+Hj9933@users.noreply.github.com>
2024-01-30 14:08:28 +0900

    [Silver IV] Title: 수 찾기, Time: 500 ms, Memory: 47416 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,20 +1,25 @@
-n = int(input())
-a = list(map(int, input().split()))
-m = int(input())
-b = list(map(int, input().split()))
-a.sort()
+import sys
+input = sys.stdin.readline
 
-for i in range(m):
-    l = 0
-    u = n - 1
-    idx = 0
-    while l <= u:
-        mid = (l + u) // 2
-        if a[mid] == b[i]:
-            idx = 1
-            break
-        elif a[mid] < b[i]:
-            l = mid + 1
-        else:
-            u = mid - 1
-    print(idx)
+n = int(input())
+A = list(map(int, input().split()))
+m = int(input())
+lst = list(map(int, input().split()))
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start+end)//2
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+A.sort()
+for i in lst:
+    if binary_search(A, i, 0, n-1) != None:
+        print(1)
+    else:
+        print(0)
