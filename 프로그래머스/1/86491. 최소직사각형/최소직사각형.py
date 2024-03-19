9e22f564c04eeba26546e0e8370f1679d0b6a960
Heejung Jang <112256988+Hj9933@users.noreply.github.com>
2024-03-19 16:04:07 +0900

    [level 1] Title: 최소직사각형, Time: 0.76 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,10 @@
+def solution(sizes):
+    w_max, h_max = 0,0
+    for w, h in sizes:
+        if w > h:
+            w, h = h, w
+        if w_max < w:
+            w_max = w
+        if h_max < h:
+            h_max = h
+    return w_max * h_max
