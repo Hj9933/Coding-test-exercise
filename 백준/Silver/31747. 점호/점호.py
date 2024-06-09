f27f60328d0ab05e2456ab4e6f0959c1e1b36daa
Heejung Jang <112256988+Hj9933@users.noreply.github.com>
2024-06-09 20:31:48 +0900

    [Silver III] Title: 점호, Time: 176 ms, Memory: 34884 KB, Score: 40 point -BaekjoonHub

--- 백준/Silver/31747. 점호/점호.py
+++ 백준/Silver/31747. 점호/점호.py
@@ -6,37 +6,48 @@ lst = list(map(int, input().split()))
 from collections import Counter
 cnt = Counter(lst)
 
-pointer = k
+pointer = k-1
+l = Counter(lst[:pointer])
 i = 0
 one = 0
 two = 0
+flag = False
 while True:
     i += 1
-    l = Counter(lst[:pointer])
-    l[1] -= one
-    l[2] -= two
+    if flag == True:
+        if pointer < n-1:
+            l[lst[pointer-1]] += 1
+            l[lst[pointer]] += 1
+        elif pointer < n:
+            l[lst[pointer]] += 1
+    else:
+        if pointer < n:
+            l[lst[pointer]] += 1
+
+    flag = False
     if l[1] > 0 and l[2] == 0:
-        cnt[1] -= 1
         one += 1
+        l[1] -= 1
         pointer += 1
         if pointer > n:
             pointer = n
     elif l[1] == 0 and l[2] > 0:
-        cnt[2] -= 1
         two += 1
+        l[2] -= 1
         pointer += 1
         if pointer > n:
             pointer = n
     elif l[1] > 0 and l[2] > 0:
-        cnt[1] -= 1
-        cnt[2] -= 1
         one += 1
         two += 1
+        l[1] -= 1
+        l[2] -= 1
         pointer += 2
+        flag = True
         if pointer > n:
             pointer = n
 
-    if sum(cnt.values()) == 0:
+    if sum(l.values()) == 0:
         break
 
 print(i)
