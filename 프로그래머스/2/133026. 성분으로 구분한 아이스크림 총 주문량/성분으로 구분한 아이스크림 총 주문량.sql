28bb9b5a2b75b5766547321cdb540a3f474913ec
Heejung Jang <112256988+Hj9933@users.noreply.github.com>
2025-02-08 07:57:39 +0900

    [level 2] Title: 성분으로 구분한 아이스크림 총 주문량, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
+++ 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
@@ -1,5 +1,6 @@
-SELECT I.INGREDIENT_TYPE, SUM(F.TOTAL_ORDER) AS TOTAL_ORDER
-FROM FIRST_HALF F
-JOIN ICECREAM_INFO I ON F.FLAVOR = I.FLAVOR
-GROUP BY I.INGREDIENT_TYPE
-ORDER BY TOTAL_ORDER ASC;
+SELECT a.INGREDIENT_TYPE, sum(total_order) as TOTAL_ORDER
+from icecream_info a
+inner join first_half b
+on a.flavor = b.flavor
+group by a.ingredient_type
+order by TOTAL_ORDER asc
