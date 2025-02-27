778ee1e70e79dbc345884651ffd48e835bc43086
Heejung Jang <112256988+Hj9933@users.noreply.github.com>
2025-02-27 23:37:08 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 760 ms, Memory: 52140 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+n = int(input())
+graph = list(list(map(int, input().split())) for _ in range(n))
+
+dp = list([-1]*n for _ in range(n))
+
+dx = [1,-1,0,0]
+dy = [0,0,-1,1]
+
+def dfs(x,y):
+    if dp[x][y] != -1:
+        return dp[x][y]
+    dp[x][y] = 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] > graph[x][y]:
+            dp[x][y] = max(dp[x][y], dfs(nx,ny)+1)
+    return dp[x][y]
+
+result = 0
+for i in range(n):
+    for j in range(n):
+        result = max(result, dfs(i,j))
+print(result)
