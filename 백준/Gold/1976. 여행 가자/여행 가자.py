6c2a886737a6207acf3712d81502825a75c0937d
Heejung Jang <112256988+Hj9933@users.noreply.github.com>
2025-03-31 15:14:59 +0900

    [Gold IV] Title: 여행 가자, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,32 +1,33 @@
 import sys
 input = sys.stdin.readline
-# 여행가자
 n = int(input())
 m = int(input())
+## 유니온 파인드
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+def union(a, b):
+    roota = find(a)
+    rootb = find(b)
+    if roota != rootb:
+        parent[rootb] = roota
 
-## 플루이드 워셜
-graph = [[0]*(n+1)]
+parent = [i for i in range(n)]
+
 for i in range(n):
-    graph.append([0]+list(map(int,input().split())))
-destination = list(map(int, input().split()))
+    tmp = list(map(int,input().split()))
+    for j in range(n):
+        if tmp[j] == 1:
+            union(i,j)
+plan = list(map(int, input().split()))
+root = find(plan[0]-1)
 
-for k in range(1, n+1):
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            if graph[i][k] == 1 and graph[k][j] == 1:
-                graph[i][j] = 1
-for i in range(1,n+1):
-    graph[i][i] = 1
-
-start = destination[0]
 flag = True
-for i in range(1, len(destination)):
-    x = destination[i]
-    if graph[start][x] == 0:
+for city in plan:
+    if find(city-1) != root:
+        print("NO")
         flag = False
         break
-    start = destination[i]
 if flag:
     print("YES")
-else:
-    print("NO")
